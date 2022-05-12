5f45d754f32990233c1c322eea0d60ad5b0c7aa5
Yantimir <mingazitdinov@mail.ru>
2022-05-12 22:56:32 +0400

    2022.05.12 добавил input для поиска

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
-import { Header } from "./components/Header/Header"
+import { Controls } from "./components/Controls/Controls";
+import { Header } from "./components/Header/Header";
+import { Main } from "./components/Main/Main";
+
 
 function App() {
     return (
         <>
             <Header />
+            <Main>
+                <Controls />
+            </Main>
         </>
     );
 }

05ff03979427057095c82cca0030d2d7647d996a
Yantimir <mingazitdinov@mail.ru>
2022-05-14 23:21:18 +0400

    fix

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 import './index.css';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
-      {/* <BrowserRouter> */}
+    <BrowserRouter>
       <App />
-      {/* </BrowserRouter> */}
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
